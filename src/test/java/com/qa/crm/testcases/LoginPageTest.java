4631297422fa3b164d778747b94fc135dc17bd2e
pallavibk <pallavibk@github.com>
2019-07-01 00:47:29 +0530

    First Commit

---
+++ src/test/java/com/qa/crm/testcases/LoginPageTest.java
@@ -0,0 +1,48 @@
+package com.qa.crm.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.qa.crm.base.TestBase;
+import com.qa.crm.pages.HomePage;
+import com.qa.crm.pages.LoginPage;
+
+public class LoginPageTest  extends TestBase
+{
+	LoginPage loginPage;
+	HomePage homePage;
+	
+	public LoginPageTest(){
+		super();
+	}
+	
+	@BeforeMethod
+	public void setUp(){
+		initialization();
+		loginPage = new LoginPage();	
+	}
+		
+	@Test(priority=1)
+	public void loginPageTitleTest(){
+	String title =loginPage.validateLoginPageTitle();
+	Assert.assertEquals(title, "Free CRM software for any Business");
+	}
+	
+	@Test(priority=2)
+	public void crmLogoImageTest(){
+		boolean flag = loginPage.validateCRMImage();
+		Assert.assertTrue(flag);
+	}
+	
+	@Test(priority=3)
+	public void loginTest(){
+		homePage = loginPage.login(prop.getProperty("username"), prop.getProperty("password"));
+	}
+	
+	@AfterMethod
+	public void tearDown(){
+		driver.quit();
+	}
+}
